1d3b5e35d1557f8360fd21002a99a2c0803da4e5
Rachel <BlantonAlmeida@gmail.com>
2016-07-03 00:11:38 -0700

    adding a custom splash with text FadeInOut

--- app/app.js
+++ app/app.js
@@ -1,2 +1,11 @@
 
   var app = angular.module('twitterApp', ['ngSanitize','twitterApp.services']);
+
+
+  $(function(){
+     setTimeout(function() {
+        $("#splash").fadeOut("slow", function() {
+           $("#content").show();
+        });
+     }, 5000);
+  });
